e0e69cf7d872c334d43858ad53429cd75084c161
Akhmad Wildan <wildanakhmad3584@gmail.com>
2021-06-13 21:35:49 +0700

    belajar http node sederhana

---
+++ app.js
@@ -0,0 +1,39 @@
+const http = require('http');
+const fs = require('fs');
+
+const renderHTML = (path, res) => {
+    fs.readFile( path , (error, data) =>{
+        if(error){
+            res.writeHead(404);
+            res.write('ERORR: File Not Found');
+        } else{
+            res.write(data);
+        }
+
+        res.end();
+    });
+}
+
+
+http
+.createServer((req, res) => {
+    
+    
+    res.writeHead(200, {
+        'Content-Type': 'text/html',
+    });
+
+    const url = req.url;
+    if(url === '/about'){
+        res.write('<h1>Ini adalah Halaman About!</h1>');
+        res.end();
+    } else if (url === '/contact'){
+        res.write('<h1>Ini adalah Halaman Contact!</h1>');
+        res.end();
+    } else {
+        renderHTML('./index.html', res);
+    }
+})
+.listen(3000, () => {
+    console.log('Server is listening on port 3000');
+});
